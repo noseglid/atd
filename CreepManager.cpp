bb466d6f10391754ac4d1d74e9662fa953ef48c5
Alexander Olsson <noseglid@gmail.com>
2013-07-06 16:57:58 +0200

    CreepManager: minor calculation optimization

--- CreepManager.cpp
+++ CreepManager.cpp
@@ -122,10 +122,11 @@ std::vector<Creep*>
 CreepManager::creeps_inside_circle(Vector3 center, float radius)
 {
   std::map<float, Creep*> sorted_result;
+  float radius_squared = radius * radius;
   for (Creep *c : creeps) {
     Vector3 delta = c->get_position() - center;
     delta *= delta;
-    if (delta.x + delta.z <= radius * radius && 0 < c->get_health()) {
+    if (delta.x + delta.z <= radius_squared) {
       sorted_result.insert(std::make_pair(c->get_distance_moved(), c));
     }
   }
