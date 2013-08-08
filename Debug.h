f64f9af23b28e1ec989cb0b2123f61e5c261939b
Alexander Olsson <noseglid@gmail.com>
2013-08-08 21:02:26 +0200

    Debug: Add colors to output

--- Debug.h
+++ Debug.h
@@ -8,9 +8,17 @@
 
 #if DEBUG == 1
 
-#define DBG(args)              __OUTPUT(std::cout, "DEBUG: " << args)
-#define DBGWRN(args)           __OUTPUT(std::cout, "WARNING: " << args)
-#define DBGERR(args)           __OUTPUT(std::cerr, "ERROR: " << args)
+#define BLUE    "\033[0;34m"
+#define BBLUE   "\033[1;34m"
+#define RED     "\033[0;31m"
+#define BRED    "\033[1;31m"
+#define YELLOW  "\033[0;33m"
+#define BYELLOW "\033[1;33m"
+#define RESET   "\033[0m"
+
+#define DBG(args)              __OUTPUT(std::cout, BBLUE   "[DEBUG] "   BLUE   << args << RESET)
+#define DBGWRN(args)           __OUTPUT(std::cout, BYELLOW "[WARNING] " YELLOW << args << RESET)
+#define DBGERR(args)           __OUTPUT(std::cerr, BRED    "[ERROR] "   RED    << args << RESET)
 
 #else
 
